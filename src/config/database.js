c40ee8377495be168a1577dc7f1411370a971f59
Fabio DrizZt <ing.fabio.arg@gmail.com>
2025-12-09 23:04:21 -0300

    Actualización para despliegue

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    process.loadEnvFile() // carga variables de entorno
+    const ENV = process.env.NODE_ENV || 'development'
+    /* const dotenv = require('dotenv')
+    dotenv.config({ path: `.env.${ENV}` }) */
+    process.loadEnvFile(`.env.${ENV}`) // carga variables de entorno
     const { DB_PROTOCOL, DB_HOST, DB_PASS, DB_USER, DB_OPTIONS, DB_NAME } =
       process.env;
-    const MONGODB_URI = `${DB_PROTOCOL}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`
+    const MONGODB_URI = ENV === 'development'
+      ? `${DB_PROTOCOL}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`
+      : `${DB_PROTOCOL}://${DB_USER}:${DB_PASS}@${DB_HOST}`
     await mongoose.connect(MONGODB_URI)
     console.log('Conectado a la base de datos')
   } catch (error) {
